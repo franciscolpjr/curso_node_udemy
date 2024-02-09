4bcf14203b70aa419f6fcfb02becc9c409aa76b3
Francisco Lazaro Pereira Junior <franciscolpjr@gmail.com>
2024-02-09 11:17:14 -0300

    Requisitando parametros com GET

--- app.js
+++ app.js
@@ -6,17 +6,13 @@ app.get("/", (req,res) => {
     res.send("Hello World");
 })
 
-app.get("/user", (req,res) => {
+app.get("/user/:nome/sobrenome/:sobrenome", (req,res) => {
     res.send({
-        usuário:"José"
+        nome : req.params.nome,
+        sobrenome : req.params.sobrenome
     });
 })
 
-app.delete("/user", (req,res) => {
-    res.send({
-        usuário:"José Deletado"
-    });
-})
 app.listen(8080, ()=>{
     console.log("Servidor rodando.")
 })
