0bd03db941b3c8633d9d99006c92d2fd4ddcf184
Francisco Lazaro Pereira Junior <franciscolpjr@gmail.com>
2024-02-10 23:14:41 -0300

    Ajustes config Eslint

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/user/:nome/sobrenome/:sobrenome', (req, res) => {
+  res.send({
+    nome: req.params.nome,
+    sobrenome: req.params.sobrenome,
+  });
+});
+
+app.listen(8080, () => {
+  console.log('Servidor rodando.');
+});
